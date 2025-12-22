ab60bf821fa5803236346e6047ed347d99a5678d
siva-netizen <sivasabarivel008@gmail.com>
2025-12-22 12:27:21 +0530

    feat: add config.js to store Appwrite API URL

--- extension/client/config.js
+++ extension/client/config.js
@@ -1,7 +1,7 @@
 // config.js
 const CONFIG = {
     // Hardcoded API URL for Appwrite Cloud Backend
-    API_URL: "https://6948346f001194e559d2.nyc.appwrite.run/refine"
+    API_URL: "https://6948e463003a88836e0a.nyc.appwrite.run/refine"
 };
 
 // Export for use in background service worker (if modules used) or global scope
